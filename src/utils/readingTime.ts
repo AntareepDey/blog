be341fe086533c1d86e0d84e46f50d85f022894c
Antareep Dey <antareep175@hotmail.com>
2025-06-20 15:38:28 +0530

    beta version

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -1,5 +1,5 @@
 export function calculateReadingTime(content: string): number {
-  const wordsPerMinute = 200;
+  const wordsPerMinute = 250;
   const words = content.trim().split(/\s+/).length;
   const readingTime = Math.ceil(words / wordsPerMinute);
   return readingTime;
